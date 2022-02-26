0cbe9ba80556e51973fde669aa0825641540798c
Sushantdbg <sushantkumardbg@gmai.com>
2022-02-26 17:44:40 +0530

    add logo, initial nabar structure

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function Navbar(){
     return(
         <>
-        <p>This is text from the navbar</p>
+        <div className="navbar--all">
+        <nav className="navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src={process.env.PUBLIC_URL + '/ncblogo.webp'} alt="" width="30" height="24" className="d-inline-block align-text-top" />
+                    NCB EXPRESS</a></div></nav>
+        </div>
         </>
     )
 }
